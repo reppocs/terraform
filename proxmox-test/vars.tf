f09d9b2bc82bca993fd9629dcb88166f3ae98493
Corey Reppond <reppocs@gmail.com>
2024-04-01 16:21:10 -0400

    moved all vars to terraform.tfvars

--- proxmox-test/vars.tf
+++ proxmox-test/vars.tf
@@ -1,6 +1,4 @@
 variable "url" {
-  description = "proxmox api address url"
-  default = "https://192.168.1.70:8006/api2/json"
 }
 
 variable "token_secret" {
@@ -10,7 +8,4 @@ variable "token_id" {
 }
 
 variable "ssh_pub_key" {
-  default = <<EOF
-  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVdqDua/NOpQk7BFxIA1IYjcfx83X2rWy7NCPKhhdE0 reppocs@gmail.com
-  EOF
 }
