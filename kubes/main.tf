ec285bfe3b3b85fbe4909bc7e29630db06fd2ec5
Corey Reppond <reppocs@gmail.com>
2025-09-30 22:38:48 -0400

    split out var files and updated kubes to new provisioner

--- kubes/main.tf
+++ kubes/main.tf
@@ -1,125 +1,80 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
-      #latest version as of Nov 30 2022
-      version = "2.9.14"
+      source  = "bpg/proxmox"
+      version = "~> 0.50"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url = var.url
-  pm_api_token_id = var.token_id
-  pm_api_token_secret = var.token_secret
-  pm_tls_insecure = true
+  endpoint = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure = true
+  
+  ssh {
+    agent    = true
+    username = "root"
+  }
 }
 
-# Creates a proxmox_vm_qemu entity named tftest
-resource "proxmox_vm_qemu" "kubes-vm-1" {
-  name = "kubes-vm-1"
-  target_node = "pve"
-  clone = "ubuntu-22.04-template"
-  full_clone  = "true"
-
-  agent = 0
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-
-  disk {
-    slot = 0
-    size = "20G"
-    type = "scsi"
-    format = "raw"
-    storage = "local-data"
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  count       = 3
+  name        = "kubes-vm-${count.index + 1}"
+  node_name   = "pve"
+  vm_id       = 170 + count.index
+  
+  clone {
+    vm_id = 9000  # Your template VM ID
   }
-
-  network {
-    model = "virtio"
+  
+  cpu {
+    cores = 2
+  }
+  
+  memory {
+    dedicated = 2048
+  }
+  
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 25
+  }
+  
+  network_device {
     bridge = "vmbr0"
   }
-
-  vmid = "131"
-  ipconfig0 = "ip=192.168.1.131/24,gw=192.168.1.1"
-  nameserver = "192.168.1.100"
-  searchdomain = "home.lab"
-  sshkeys = var.ssh_pub_key
-
+  
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "192.168.1.${170 + count.index}/24"
+        gateway = "192.168.1.1"
+      }
+    }
+    
+    dns {
+      servers = ["8.8.8.8", "8.8.4.4"]
+    }
+    
+    user_account {
+      username = "ubuntu"
+      password = var.vm_password
+      keys     = [
+        trimspace(file("~/.ssh/id_ed25519.pub"))
+      ]
+    }
+  }
+  
+  operating_system {
+    type = "l26"
+  }
 }
 
-resource "proxmox_vm_qemu" "kubes-vm-2" {
-  name = "kubes-vm-2"
-  target_node = "pve"
-  clone = "ubuntu-22.04-template"
-  full_clone = "true"
-
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-
-  disk {
-    slot = 0
-    size = "20G"
-    type = "scsi"
-    format = "raw"
-    storage = "local-data"
+output "vm_ips" {
+  value = {
+    for vm in proxmox_virtual_environment_vm.ubuntu_vm :
+    vm.name => vm.ipv4_addresses[1][0]
   }
-
-  network {
-    model = "virtio"
-    bridge = "vmbr0"
-  }
-
-  vmid = "132"
-  ipconfig0 = "ip=192.168.1.132/24,gw=192.168.1.1"
-  nameserver = "192.168.1.100"
-  searchdomain = "home.lab"
-  sshkeys = var.ssh_pub_key
-
-}
-
-resource "proxmox_vm_qemu" "kubes-vm-3" {
-  name = "kubes-vm-3"
-  target_node = "pve"
-  clone = "ubuntu-22.04-template"
-  full_clone = "true"
-
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-
-  disk {
-    slot = 0
-    size = "20G"
-    type = "scsi"
-    format = "raw"
-    storage = "local-data"
-  }
-
-  network {
-    model = "virtio"
-    bridge = "vmbr0"
-  }
-
-  vmid = "133"
-  ipconfig0 = "ip=192.168.1.133/24,gw=192.168.1.1"
-  nameserver = "192.168.1.100"
-  searchdomain = "home.lab"
-  sshkeys = var.ssh_pub_key
-
 }
--- mytest/main.tf
+++ mytest/main.tf
@@ -7,23 +7,6 @@ terraform {
   }
 }
 
-variable "proxmox_endpoint" {
-  description = "Proxmox API endpoint URL"
-  type        = string
-}
-
-variable "proxmox_api_token" {
-  description = "Proxmox API token"
-  type        = string
-  sensitive   = true
-}
-
-variable "vm_password" {
-  description = "Password for the VM user account"
-  type        = string
-  sensitive   = true
-}
-
 provider "proxmox" {
   endpoint = var.proxmox_endpoint
   api_token = var.proxmox_api_token
