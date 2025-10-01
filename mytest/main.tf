0d75a6f54237f678f6ba8f795fb0797b8a95c590
Corey Reppond <reppocs@gmail.com>
2025-09-30 22:03:55 -0400

    removed old stuff and added new stuff

---
+++ mytest/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.50"
+    }
+  }
+}
+
+variable "proxmox_endpoint" {
+  description = "Proxmox API endpoint URL"
+  type        = string
+}
+
+variable "proxmox_api_token" {
+  description = "Proxmox API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "vm_password" {
+  description = "Password for the VM user account"
+  type        = string
+  sensitive   = true
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure = true
+  
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name        = "ubuntu-vm"
+  node_name   = "pve"
+  vm_id       = 160
+  
+  clone {
+    vm_id = 9000  # Your template VM ID
+  }
+  
+  cpu {
+    cores = 2
+  }
+  
+  memory {
+    dedicated = 2048
+  }
+  
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 25
+  }
+  
+  network_device {
+    bridge = "vmbr0"
+  }
+  
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "192.168.1.160/24"
+        gateway = "192.168.1.1"
+      }
+    }
+    
+    dns {
+      servers = ["8.8.8.8", "8.8.4.4"]
+    }
+    
+    user_account {
+      username = "ubuntu"
+      password = var.vm_password
+      keys     = [
+        trimspace(file("~/.ssh/id_ed25519.pub"))
+      ]
+    }
+  }
+  
+  operating_system {
+    type = "l26"
+  }
+}
+
+output "vm_ip" {
+  value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses[1][0]
+}
